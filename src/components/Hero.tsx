3433cb586f6454efc5e352fb601e8243f177bd6a
Rohit Thakur <rohitkumarsengar2002@gmail.com>
2025-10-28 12:16:28 +0000

    increase the scroll speed .

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -73,7 +73,7 @@ const Hero = () => {
     offset: ['start start', 'end start'],
   });
   
-  const imageY = useTransform(scrollYProgress, [0, 1], [0, 200]);
+  const imageY = useTransform(scrollYProgress, [0, 1], [0, 400]);
 
   useEffect(() => {
     const interval = setInterval(() => {
