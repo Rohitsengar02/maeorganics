56f9a536f9a9a52e97494f9238d9a6b9e587a680
Rohit Thakur <rohitkumarsengar2002@gmail.com>
2025-10-29 06:39:07 +0000

    now update the categories component add infinite auto scroll and add use

--- src/components/PromoBanner.tsx
+++ src/components/PromoBanner.tsx
@@ -7,7 +7,7 @@ const PromoBanner = forwardRef<HTMLDivElement>((props, ref) => {
   return (
     <section
       ref={ref}
-      className="w-full relative h-[25vh] md:h-[70vh] lg:h-[100vh]"
+      className="w-full relative h-[25vh] md:h-[70vh] lg:h-[70vh]"
     >
       <Image
         src="https://res.cloudinary.com/ds1wiqrdb/image/upload/v1761715542/Beige_and_Black_Modern_Elegant_Fashion_Email_Header_1_mqmppk.png"
